f9212a75c341bb205aa13d302f54c23f960b58ad
Marian <69646823+Marian06@users.noreply.github.com>
2020-10-20 22:04:07 +0200

    v0.6.1

--- src/main/java/com/myra/dev/marian/listeners/autorole/AutoroleAssign.java
+++ src/main/java/com/myra/dev/marian/listeners/autorole/AutoroleAssign.java
@@ -1,7 +1,9 @@
 package com.myra.dev.marian.listeners.autorole;
 
+import com.myra.dev.marian.database.Prefix;
 import com.myra.dev.marian.database.allMethods.Database;
 import com.myra.dev.marian.utilities.management.Events;
+import com.myra.dev.marian.utilities.management.Manager;
 import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 
@@ -13,6 +15,10 @@ public class AutoroleAssign extends Events {
         if (!db.getListenerManager().check("autoRole")) return;
         //get role
         Role autoRole = event.getGuild().getRoleById(db.get("autoRole"));
+        // Check if no role is set
+        if (autoRole.equals("not set")) {
+            Manager.getUtilities().error(event.getGuild().getDefaultChannel(), "autorole", "\uD83D\uDCDD", "You didn't specify a autorole", "To indicate a autorole, type in `" + Prefix.getPrefix(event.getGuild()) + "autorole <role>`", event.getGuild().getIconUrl());
+        }
         //assign role
         event.getGuild().addRoleToMember(event.getMember(), autoRole).queue();
     }
