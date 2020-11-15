6144442218a0d8b8703b0f8faa1d45e469197b7d
Marian <69646823+Marian06@users.noreply.github.com>
2020-11-15 17:15:48 +0100

    v1.0.1 [Changed prefix reset to ~prefix RESET]

--- src/main/java/com/myra/dev/marian/commands/administrator/Prefix.java
+++ src/main/java/com/myra/dev/marian/commands/administrator/Prefix.java
@@ -1,12 +1,10 @@
 package com.myra.dev.marian.commands.administrator;
 
-import com.myra.dev.marian.Bot;
 import com.myra.dev.marian.database.allMethods.Database;
-
-import com.myra.dev.marian.utilities.Utilities;
 import com.myra.dev.marian.management.commands.Command;
 import com.myra.dev.marian.management.commands.CommandContext;
 import com.myra.dev.marian.management.commands.CommandSubscribe;
+import com.myra.dev.marian.utilities.Utilities;
 import net.dv8tion.jda.api.EmbedBuilder;
 
 @CommandSubscribe(
@@ -27,27 +25,19 @@ public class Prefix implements Command {
             return;
         }
 // Change the prefix
+        // New prefix
+        String newPrefix = ctx.getArguments()[0];
+        if (newPrefix.equals("RESET")) newPrefix = "~";
+
         Database db = new Database(ctx.getGuild());
         // Change prefix
-        db.set("prefix", ctx.getArguments()[0]);
+        db.set("prefix", newPrefix);
         //success information
         Utilities.getUtils().success(ctx.getChannel(),
                 "prefix", "\uD83D\uDCCC",
                 "Prefix changed",
-                "Prefix changed to `" + new Database(ctx.getGuild()).get("prefix") + "`",
+                "Prefix changed to `" + newPrefix + "`",
                 ctx.getAuthor().getEffectiveAvatarUrl(),
                 false, null);
-        //prefix reset
-        if (ctx.getEvent().getMessage().getContentRaw().equalsIgnoreCase(ctx.getEvent().getJDA().getSelfUser().getAsMention().replace("<@", "<@!") + "prefix")) {
-            //Database
-            db.set("prefix", Bot.prefix);
-            //success info
-            Utilities.getUtils().success(ctx.getChannel(),
-                    "prefix", "\uD83D\uDCCC",
-                    "Prefix reset",
-                    "Prefix changed to `" + Bot.prefix + "`",
-                    ctx.getAuthor().getEffectiveAvatarUrl(),
-                    false, null);
-        }
     }
 }
