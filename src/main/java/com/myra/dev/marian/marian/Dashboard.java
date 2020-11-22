e387250e468f087ed3d8138ae4af932da68b68df
Marian <69646823+Marian06@users.noreply.github.com>
2020-11-22 18:21:28 +0100

    v1.0.1 [Dashboard shows now total shards]

--- src/main/java/com/myra/dev/marian/marian/Dashboard.java
+++ src/main/java/com/myra/dev/marian/marian/Dashboard.java
@@ -19,7 +19,7 @@ public class Dashboard implements Command {
                 .setAuthor("dashboard", null, ctx.getAuthor().getEffectiveAvatarUrl())
                 .addField("CPU usage", resources.getCpuLoad(), false)
                 .addField("RAM usage", resources.getRAMUsage(), false)
-                .addField("Shard", String.valueOf(ctx.getEvent().getJDA().getShardInfo().getShardId()), false)
+                .addField("Shards", String.valueOf(ctx.getEvent().getJDA().getShardManager().getShardsTotal()), false)
                 .addField("Guilds", String.valueOf(ctx.getEvent().getJDA().getGuilds().size()), false);
         ctx.getChannel().sendMessage(dashboard.build()).queue();
     }
