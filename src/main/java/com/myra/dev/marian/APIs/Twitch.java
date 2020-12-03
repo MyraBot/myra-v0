0d1b4574361a6721ae7ceeef4317e095ada6538c
Marian <69646823+Marian06@users.noreply.github.com>
2020-12-03 18:39:26 +0100

    v1.0.3 [Added the Twitch.getChannel method to the Twitch.java class]

--- src/main/java/com/myra/dev/marian/APIs/Twitch.java
+++ src/main/java/com/myra/dev/marian/APIs/Twitch.java
@@ -15,7 +15,7 @@ import java.time.format.DateTimeFormatter;
 public class Twitch {
     public static String accessToken;
 
-    //get access token
+    // Get access token
     public void jdaReady(ReadyEvent event) throws IOException {
         //form parameters
         RequestBody body = new FormBody.Builder()
@@ -57,7 +57,34 @@ public class Twitch {
         }
     }
 
-    //request stream
+    // Get user
+    public JSONObject getChannel(String name) throws Exception {
+        //search channel request
+        Request channel = new Request.Builder()
+                .addHeader("client-id", Utilities.getUtils().twitchClientId)
+                .addHeader("Authorization", "Bearer " + Twitch.accessToken)
+                .url("https://api.twitch.tv/helix/search/channels?query=" + name)
+                .build();
+        //execute call
+        String channelOutput = null;
+        try (Response channelResponse = Utilities.HTTP_CLIENT.newCall(channel).execute()) {
+            channelOutput = channelResponse.body().string();
+        }
+        //create Json object
+        org.json.JSONObject JsonChannel = new org.json.JSONObject(channelOutput);
+        //if no channel found
+        if (JsonChannel.getJSONArray("data").length() == 0) {
+            System.out.println("no user found");
+            return new org.json.JSONObject();
+        }
+        org.json.JSONObject channelData = JsonChannel.getJSONArray("data").getJSONObject(0);
+        //search values
+        String user = channelData.getString("display_name");
+        String profilePicture = channelData.getString("thumbnail_url");
+        return new JSONObject().put("user", user).put("profilePicture", profilePicture);
+    }
+
+    // Get stream
     public JSONObject getStream(String channelName) throws IOException {
         // Create channel http get request
         Request channel = new Request.Builder()
