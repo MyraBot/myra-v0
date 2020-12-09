e482fe82636c93a42611374b0f99098de7a0cd6b
Marian <69646823+Marian06@users.noreply.github.com>
2020-12-09 18:49:36 +0100

    v1.0.4 [Fixed error, if user hasn't logged in into discord.boats yet]

--- src/main/java/com/myra/dev/marian/utilities/APIs/DiscordBoats.java
+++ src/main/java/com/myra/dev/marian/utilities/APIs/DiscordBoats.java
@@ -21,6 +21,8 @@ public class DiscordBoats {
                 .url("https://discord.boats/api/bot/" + Bot.myra + "/voted?id=" + user.getId())
                 .build();
         final String response = Utilities.HTTP_CLIENT.newCall(request).execute().body().string(); // Get response
+        if (new JSONObject(response).getString("message").equals("User wasn't found"))
+            return false; // User hasn't logged in in discord.boats yet
         return new JSONObject(response).getBoolean("voted"); // Return voted state
     }
 }
