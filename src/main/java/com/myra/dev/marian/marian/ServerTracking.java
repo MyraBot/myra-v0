eada248a75d2ce5c0be016b17f9b0e1dfde71c17
Marian <69646823+Marian06@users.noreply.github.com>
2020-12-15 20:18:58 +0100

    v1.0.4 [Added server join date and changed the tracking channel]

--- src/main/java/com/myra/dev/marian/marian/ServerTracking.java
+++ src/main/java/com/myra/dev/marian/marian/ServerTracking.java
@@ -1,19 +1,21 @@
 package com.myra.dev.marian.marian;
 
 
+import com.myra.dev.marian.Bot;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
 
-public class ServerTracking  {
+public class ServerTracking {
 
     public void guildJoinEvent(GuildJoinEvent event) {
-        EmbedBuilder server = new EmbedBuilder();
-        server.setTitle("I joined " + event.getGuild().getName());
-        server.setThumbnail(event.getGuild().getIconUrl());
-        server.addField("\uD83D\uDC51 │ owner ", event.getGuild().getOwner().getUser().getName(), true);
-        server.addField("\uD83C\uDF9F │ guild id ", event.getGuild().getId(), true);
-        server.addField("\uD83E\uDDEE │ member count", Integer.toString(event.getGuild().getMemberCount()), true);
+        EmbedBuilder server = new EmbedBuilder()
+                .setTitle("I joined " + event.getGuild().getName())
+                .setThumbnail(event.getGuild().getIconUrl())
+                .addField("\uD83D\uDC51 │ owner ", event.getGuild().getOwner().getUser().getName(), true)
+                .addField("\uD83C\uDF9F │ guild id ", event.getGuild().getId(), true)
+                .addField("\uD83E\uDDEE │ member count", Integer.toString(event.getGuild().getMemberCount()), true)
+                .setTimestamp(event.getGuild().getMember(event.getJDA().getSelfUser()).getTimeJoined().toInstant());
 
-        event.getJDA().getGuildById("642809436515074053").getTextChannelById("721004402915147847").sendMessage(server.build()).queue();
+        event.getJDA().getGuildById(Bot.marianServer).getTextChannelById("788448343927160852").sendMessage(server.build()).queue();
     }
 }
